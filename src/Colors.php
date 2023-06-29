bf1e86ae7a838eff62dc7112fb4cee0a8cfa8ed2
PeterB <peterb@beekman.nl>
2023-06-29 13:03:41 +0200

    .

--- src/Colors.php
+++ src/Colors.php
@@ -9,52 +9,71 @@
 namespace wapmorgan\NcursesObjects;
 
 
-class Colors {
-    /**
-     * No color (black)
-     * @var int
-     */
-    const COLOR_BLACK = NCURSES_COLOR_BLACK;
+class Colors
+{
+    protected static $pairs = [];
 
-    /**
-     * White
-     * @var int
-     */
-    const COLOR_WHITE = NCURSES_COLOR_WHITE;
+    const YELLOW = 1;
+    const CYAN = 2;
+    const WHITE = 3;
+    const GREEN = 4;
+    const RED = 5;
+    const BLACK = 6;
+    const BLUE = 7;
+    const MAGENTA = 8;
 
-    /**
-     * Red - supported when terminal is in color mode
-     * @var int
-     */
-    const COLOR_RED = NCURSES_COLOR_RED;
+    const YELLOW_BLACK = 9;
+    const CYAN_BLACK = 10;
+    const WHITE_BLACK = 11;
+    const GREEN_BLACK = 12;
+    const RED_BLACK = 13;
+    const BLACK_BLACK = 14;
+    const BLUE_BLACK = 15;
+    const MAGENTA_BLACK = 16;
 
-    /**
-     * Green - supported when terminal is in color mode
-     * @var int
-     */
-    const COLOR_GREEN = NCURSES_COLOR_GREEN;
+    const YELLOW_YELLOW = 17;
+    const CYAN_YELLOW = 18;
+    const WHITE_YELLOW = 19;
+    const GREEN_YELLOW = 20;
+    const RED_YELLOW = 21;
+    const BLACK_YELLOW = 22;
+    const BLUE_YELLOW = 23;
+    const MAGENTA_YELLOW = 24;
 
-    /**
-     * Yellow - supported when terminal is in color mode
-     * @var int
-     */
-    const COLOR_YELLOW = NCURSES_COLOR_YELLOW;
+    public static function initialize()
+    {
+        if (ncurses_has_colors()) {
+            ncurses_start_color();
+            ncurses_use_default_colors();
 
-    /**
-     * Blue - supported when terminal is in color mode
-     * @var int
-     */
-    const COLOR_BLUE = NCURSES_COLOR_BLUE;
+            ncurses_init_pair(self::YELLOW, NCURSES_COLOR_YELLOW, -1);
+            ncurses_init_pair(self::CYAN, NCURSES_COLOR_CYAN, -1);
+            ncurses_init_pair(self::WHITE, NCURSES_COLOR_WHITE, -1);
+            ncurses_init_pair(self::GREEN, NCURSES_COLOR_GREEN, -1);
+            ncurses_init_pair(self::RED, NCURSES_COLOR_RED, -1);
+            ncurses_init_pair(self::BLACK, NCURSES_COLOR_BLACK, -1);
+            ncurses_init_pair(self::BLUE, NCURSES_COLOR_BLUE, -1);
+            ncurses_init_pair(self::MAGENTA, NCURSES_COLOR_MAGENTA, -1);
 
-    /**
-     * Cyan - supported when terminal is in color mode
-     * @var int
-     */
-    const COLOR_CYAN = NCURSES_COLOR_CYAN;
+            ncurses_init_pair(self::YELLOW_BLACK, NCURSES_COLOR_YELLOW, NCURSES_COLOR_BLACK);
+            ncurses_init_pair(self::CYAN_BLACK, NCURSES_COLOR_CYAN, NCURSES_COLOR_BLACK);
+            ncurses_init_pair(self::WHITE_BLACK, NCURSES_COLOR_WHITE, NCURSES_COLOR_BLACK);
+            ncurses_init_pair(self::GREEN_BLACK, NCURSES_COLOR_GREEN, NCURSES_COLOR_BLACK);
+            ncurses_init_pair(self::RED_BLACK, NCURSES_COLOR_RED, NCURSES_COLOR_BLACK);
+            ncurses_init_pair(self::BLACK_BLACK, NCURSES_COLOR_BLACK, NCURSES_COLOR_BLACK);
+            ncurses_init_pair(self::BLUE_BLACK, NCURSES_COLOR_BLUE, NCURSES_COLOR_BLACK);
+            ncurses_init_pair(self::MAGENTA_BLACK, NCURSES_COLOR_MAGENTA, NCURSES_COLOR_BLACK);
 
-    /**
-     * Magenta - supported when terminal is in color mode
-     * @var int
-     */
-    const COLOR_MAGENTA = NCURSES_COLOR_MAGENTA;
-} 
+            ncurses_init_pair(self::YELLOW_YELLOW, NCURSES_COLOR_YELLOW, NCURSES_COLOR_YELLOW);
+            ncurses_init_pair(self::CYAN_YELLOW, NCURSES_COLOR_CYAN, NCURSES_COLOR_YELLOW);
+            ncurses_init_pair(self::WHITE_YELLOW, NCURSES_COLOR_WHITE, NCURSES_COLOR_YELLOW);
+            ncurses_init_pair(self::GREEN_YELLOW, NCURSES_COLOR_GREEN, NCURSES_COLOR_YELLOW);
+            ncurses_init_pair(self::RED_YELLOW, NCURSES_COLOR_RED, NCURSES_COLOR_YELLOW);
+            ncurses_init_pair(self::BLACK_YELLOW, NCURSES_COLOR_BLACK, NCURSES_COLOR_YELLOW);
+            ncurses_init_pair(self::BLUE_YELLOW, NCURSES_COLOR_BLUE, NCURSES_COLOR_YELLOW);
+            ncurses_init_pair(self::MAGENTA_YELLOW, NCURSES_COLOR_MAGENTA, NCURSES_COLOR_YELLOW);
+        }
+    }
+
+
+}
--- src/Window.php
+++ src/Window.php
@@ -1,219 +1,278 @@
 <?php
+
 namespace wapmorgan\NcursesObjects;
 
 /**
  * A window object that implements functionality for ncurses window resource
  * @author wapmorgan (wapmorgan@gmail.com)
  */
-class Window {
-	/**
-	 * ncurses window resource
-	 */
-	private $windowResource;
-	/**
-	 * associated Panel
-	 */
-	private $panel;
-	/**
-	 * cursor position
-	 */
-	private $cursorX;
-	private $cursorY;
+class Window
+{
+    /**
+     * ncurses window resource
+     */
+    private $windowResource;
+    /**
+     * associated Panel
+     */
+    private $panel;
+    /**
+     * cursor position
+     */
+    private $cursorX;
+    private $cursorY;
 
-	const BORDER_STYLE_SOLID = 1; // ┐
-	const BORDER_STYLE_DOUBLE = 2; // ╗
-	const BORDER_STYLE_BLOCK = 3; // ■
+    const BORDER_STYLE_SOLID = 1; // ┐
+    const BORDER_STYLE_DOUBLE = 2; // ╗
+    const BORDER_STYLE_BLOCK = 3; // ■
 
-	/**
-	 * window geometry
-	 */
-	private $rows;
-	private $columns;
-	private $x;
-	private $y;
+    /**
+     * window geometry
+     */
+    private $rows;
+    private $columns;
+    private $x;
+    private $y;
 
     /**
      * @var WindowStyle
      */
     protected $style;
 
-	/**
-	 * Create a window
-	 *
-	 * @param int $columns
-	 * @param int $rows
-	 * @param int $x
-	 * @param int $y
-	 */
-	public function __construct($columns = 0, $rows = 0, $x = 0, $y = 0) {
-		$this->x = $x;
-		$this->y = $y;
-		$this->windowResource = ncurses_newwin($rows, $columns, $y, $x);
-		if ($rows == 0 || $columns == 0)
-			$this->getSize($columns, $rows);
-		$this->columns = $columns;
-		$this->rows = $rows;
-	}
+    /**
+     * Create a window
+     *
+     * @param  int  $columns
+     * @param  int  $rows
+     * @param  int  $x
+     * @param  int  $y
+     */
+    public function __construct($columns = 0, $rows = 0, $x = 0, $y = 0)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->windowResource = ncurses_newwin($rows, $columns, $y, $x);
+        if ($rows == 0 || $columns == 0) {
+            $this->getSize($columns, $rows);
+        }
+        $this->columns = $columns;
+        $this->rows = $rows;
+    }
 
-	/**
-	 * Destructs a window and associated panel
-	 */
-	public function __destruct() {
-		if ($this->panel !== null)
-			$this->panel = null;
+    /**
+     * Destructs a window and associated panel
+     */
+    public function __destruct()
+    {
+        if ($this->panel !== null) {
+            $this->panel = null;
+        }
 //		ncurses_delwin($this->windowResource);
-	}
+    }
 
-	/**
-	 * Returns a ncurses window resource
-	 * @return resource
-	 */
-	public function getWindow() {
-		return $this->windowResource;
-	}
+    /**
+     * Returns a ncurses window resource
+     * @return resource
+     */
+    public function getWindow()
+    {
+        return $this->windowResource;
+    }
 
-	/**
-	 * Gets window size
-	 * @param int $columns Will be filled with window columns
-	 * @param int $rows Will be filled with window rows
-	 * @return array An array with elements 'columns' and 'rows'
-	 */
-	public function getSize(&$columns = null, &$rows = null) {
-		ncurses_getmaxyx($this->windowResource, $rows, $columns);
-		return array('columns' => $columns, 'rows' => $rows);
-	}
+    /**
+     * Gets window size
+     * @param  int  $columns  Will be filled with window columns
+     * @param  int  $rows  Will be filled with window rows
+     * @return array An array with elements 'columns' and 'rows'
+     */
+    public function getSize(&$columns = null, &$rows = null)
+    {
+        ncurses_getmaxyx($this->windowResource, $rows, $columns);
+        return array('columns' => $columns, 'rows' => $rows);
+    }
 
-	/**
-	 * Draws a border around this window
-	 * @return Window This object
-	 */
-	public function border($left = 0, $right = 0, $top = 0, $bottom = 0, $tl_corner = 0, $tr_corner = 0, $bl_corner = 0, $br_corner = 0) {
-		ncurses_wborder($this->windowResource, $left, $right, $top, $bottom, $tl_corner, $tr_corner, $bl_corner, $br_corner);
-		return $this;
-	}
+    /**
+     * Draws a border around this window
+     * @return Window This object
+     */
+    public function border($left = 0, $right = 0, $top = 0, $bottom = 0, $tl_corner = 0, $tr_corner = 0, $bl_corner = 0, $br_corner = 0)
+    {
+        ncurses_wborder($this->windowResource, $left, $right, $top, $bottom, $tl_corner, $tr_corner, $bl_corner, $br_corner);
+        return $this;
+    }
 
-	/**
-	 * Draws a border with non-usual style
-	 * @param integer $style Can be BORDER_STYLE_SOLID, BORDER_STYLE_DOUBLE or BORDER_STYLE_BLOCK
-	 * @return Window This object
-	 */
-	public function borderStyle($style) {
-		if ($style == self::BORDER_STYLE_SOLID) {
-			$this->border();
-		}
-		elseif($style == self::BORDER_STYLE_DOUBLE) {
-			$this->border(226, 186, 205, 205, 201, 187, 200, 188);
+    /**
+     * Draws a border with non-usual style
+     * @param  integer  $style  Can be BORDER_STYLE_SOLID, BORDER_STYLE_DOUBLE or BORDER_STYLE_BLOCK
+     * @return Window This object
+     */
+    public function borderStyle($style)
+    {
+        if ($style == self::BORDER_STYLE_SOLID) {
+            $this->border();
+        } elseif ($style == self::BORDER_STYLE_DOUBLE) {
+            $this->border(226, 186, 205, 205, 201, 187, 200, 188);
 //            $this->border(ord('║'), ord('║'), ord('═'), ord('═'), ord('╔'), ord('╗'), ord('╚'), ord('╝'));
-		}
-		return $this;
-	}
+        }
+        return $this;
+    }
 
-	/**
-	 * Refreshes (redraws) a window
-	 * @return Window This object
-	 */
-	public function refresh() {
-		ncurses_wrefresh($this->windowResource);
-		return $this;
-	}
+    /**
+     * Refreshes (redraws) a window
+     * @return Window This object
+     */
+    public function refresh()
+    {
+        ncurses_wrefresh($this->windowResource);
+        return $this;
+    }
 
-	/**
-	 * Draws a window title
-	 * @param string $title Title
-	 * @return Window This object
-	 */
-	public function title($title) {
-		$this->moveCursor(1, 0);
-		$this->drawStringHere($title, NCURSES_A_REVERSE);
-		return $this;
-	}
+    /**
+     * Draws a window title
+     * @param  string  $title  Title
+     * @return Window This object
+     */
+    public function title($title)
+    {
+        $this->moveCursor(1, 0);
+        $this->drawStringHere($title, NCURSES_A_REVERSE);
+        return $this;
+    }
 
-	/**
-	 * Draws a window status (a line at the bottom)
-	 * @param string $status Status
-	 * @return Window This object
-	 */
-	public function status($status) {
-		$this->moveCursor(1, $this->rows -1 );
-		$this->drawStringHere($status, NCURSES_A_REVERSE);
-		return $this;
-	}
+    /**
+     * Draws a window status (a line at the bottom)
+     * @param  string  $status  Status
+     * @return Window This object
+     */
+    public function status($status)
+    {
+        $this->moveCursor(1, $this->rows - 1);
+        $this->drawStringHere($status, NCURSES_A_REVERSE);
+        return $this;
+    }
 
-	/**
-	 * Erases a window
-	 * @return Window This object
-	 */
-	public function erase() {
-		ncurses_werase($this->windowResource);
-		return $this;
-	}
+    /**
+     * Erases a window
+     * @return Window This object
+     */
+    public function erase()
+    {
+//        ncurses_werase($this->windowResource);
 
-	/**
-	 * Moves a cursor
-	 * @param integer $x Cursor column
-	 * @param integer $y Cursor row
-	 * @return Window This object
-	 */
-	public function moveCursor($x, $y) {
-		$this->cursorX = $x;
-		$this->cursorY = $y;
-		ncurses_wmove($this->windowResource, $y, $x);
-		return $this;
-	}
+//        ncurses_wclear($this->windowResource);
+        ncurses_delwin($this->windowResource);
+//        ncurses_wrefresh($this->windowResource);
 
-	/**
-	 * Draws a string at current position
-	 * @param string $string String
-	 * @param integer $attributes Ncurses attributes (eg. NCURSES_A_REVERSE)
-	 * @see http://pubs.opengroup.org/onlinepubs/007908799/xcurses/curses.h.html for available attributes (WA_LOW => NCURSES_A_LOW)
-	 * @return Window This object
-	 */
-	public function drawStringHere($string, $attributes = 0) {
-		ncurses_wattron($this->windowResource, $attributes);
-		ncurses_waddstr($this->windowResource, $string);
-		ncurses_wattroff($this->windowResource, $attributes);
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @todo Delete
-	 */
-	public static function _debug() {
-		$msg = '';
+    /**
+     * Moves a cursor
+     * @param  integer  $x  Cursor column
+     * @param  integer  $y  Cursor row
+     * @return Window This object
+     */
+    public function moveCursor($x, $y)
+    {
+        $this->cursorX = $x;
+        $this->cursorY = $y;
+        ncurses_wmove($this->windowResource, $y, $x);
+        return $this;
+    }
 
-		foreach(func_get_args() as $arg) {
-				$msg .= var_export($arg, true) . PHP_EOL;
-		}
+    /**
+     * Draws a string at current position
+     * @param  string  $string  String
+     * @param  integer  $attributes  Ncurses attributes (eg. NCURSES_A_REVERSE)
+     * @return Window This object
+     * @see http://pubs.opengroup.org/onlinepubs/007908799/xcurses/curses.h.html for available attributes (WA_LOW => NCURSES_A_LOW)
+     */
+    public function drawStringHere($string, $attributes = 0)
+    {
+        ncurses_wattron($this->windowResource, $attributes);
+        ncurses_waddstr($this->windowResource, $string);
+        ncurses_wattroff($this->windowResource, $attributes);
+        return $this;
+    }
 
-		file_put_contents('/tmp/ncurses.log', $msg);
-	}
+    /**
+     * Draws a string at current position
+     * @param  string  $string  String
+     * @param  integer  $attributes  Ncurses attributes (eg. NCURSES_A_REVERSE)
+     * @return Window This object
+     * @see http://pubs.opengroup.org/onlinepubs/007908799/xcurses/curses.h.html for available attributes (WA_LOW => NCURSES_A_LOW)
+     */
+    public function drawString($string, $x = null, $y = null, $attributes = 0, $colorPair = null)
+    {
+        if (!is_null($colorPair) && ncurses_has_colors()) {
+//        ncurses_init_pair(1, NCURSES_COLOR_YELLOW, NCURSES_COLOR_BLUE);
+//        ncurses_wcolor_set($this->windowResource,1);
+//        ncurses_mvaddstr(10, 10, "Hello world! Yellow on blue text!");
 
-	/**
-	 * Makes a panel associated with this window
-	 * @return this
-	 */
-	public function makePanel() {
-		$this->panel = new Panel($this);
-		return $this;
-	}
+            ncurses_wcolor_set($this->windowResource, $colorPair);
+        }
 
-	/**
-	 * Returns a Panel associated with this window
-	 * @return Panel or null
-	 */
-	public function getPanel() {
-		return $this->panel;
-	}
+        if ($attributes > 0) {
+            ncurses_wattron($this->windowResource, $attributes);
+        }
+        if (!is_null($y) && !is_null($y)) {
+            ncurses_mvwaddstr($this->windowResource, $y, $x, $string);
+        } else {
+            ncurses_waddstr($this->windowResource, $string);
+        }
+        if ($attributes > 0) {
+            ncurses_wattroff($this->windowResource, $attributes);
+        }
 
-	/**
-	 * Makes a window that will be in center of the screen
-	 * @param Window $parentWindow A window that will be major for new window
-	 * @param integer $columns New window columns count
-	 * @param integer $row New window rows count
-	 * @return Window A new window that centered of parent window
-	 */
-	static public function createCenteredOf(Window $parentWindow, $columns, $rows) {
-		$parentWindow->getSize($max_columns, $max_rows);
-		return new Window($columns, $rows, ($max_columns / 2 - $columns / 2), ($max_rows / 2 - $rows / 2));
-	}
+        ncurses_wcolor_set($this->windowResource, 0);
+        ncurses_use_default_colors();
+
+        return $this;
+    }
+
+    /**
+     * @todo Delete
+     */
+    public static function _debug()
+    {
+        $msg = '';
+
+        foreach (func_get_args() as $arg) {
+            $msg .= var_export($arg, true).PHP_EOL;
+        }
+
+        file_put_contents('/tmp/ncurses.log', $msg);
+    }
+
+    /**
+     * Makes a panel associated with this window
+     * @return this
+     */
+    public function makePanel()
+    {
+        $this->panel = new Panel($this);
+        return $this;
+    }
+
+    /**
+     * Returns a Panel associated with this window
+     * @return Panel or null
+     */
+    public function getPanel()
+    {
+        return $this->panel;
+    }
+
+    /**
+     * Makes a window that will be in center of the screen
+     * @param  Window  $parentWindow  A window that will be major for new window
+     * @param  integer  $columns  New window columns count
+     * @param  integer  $row  New window rows count
+     * @return Window A new window that centered of parent window
+     */
+    static public function createCenteredOf(Window $parentWindow, $columns, $rows)
+    {
+        $parentWindow->getSize($max_columns, $max_rows);
+        return new Window($columns, $rows, ($max_columns / 2 - $columns / 2), ($max_rows / 2 - $rows / 2));
+    }
 }
